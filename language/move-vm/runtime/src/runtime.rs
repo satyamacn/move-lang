33e5250447ea5ea8516c05a49690b41bbad364a9
Dario Russi <dario@aptoslabs.com>
2022-11-14 13:36:51 -0600

    test me

--- language/move-vm/runtime/src/runtime.rs
+++ language/move-vm/runtime/src/runtime.rs
@@ -74,6 +74,7 @@ impl VMRuntime {
         _gas_meter: &mut impl GasMeter,
         compat: Compatibility,
     ) -> VMResult<()> {
+        println!("publish_module_bundle");
         // deserialize the modules. Perform bounds check. After this indexes can be
         // used with the `[]` operator
         let compiled_modules = match modules
